4166fd9939be2eefeb832d90188c12c95b94b610
Jhonatan Teixeira <jhonatan@voxtecnologia.com.br>
2017-12-11 18:18:56 -0300

    fix data type mapping with [] (brackets) notation

--- Normalizer.php
+++ Normalizer.php
@@ -37,10 +37,17 @@ class Normalizer implements NormalizerInterface
             $binding = $propertyMetadata->getAnnotation(Bindings::class);
             $value   = $propertyMetadata->getValue($object);
             
-            if ($this->supportsNormalization($value) 
-                && !in_array(spl_object_hash($value), $context['normalized'] ?? [])) {
-                $context['normalized'][] = spl_object_hash($value);
-                $value = $this->normalize($value, $format, $context);
+            if (is_array($value) 
+                && (preg_match('/\[\]$/', $propertyMetadata->type) || $propertyMetadata->type == 'array')) {
+                $items = [];
+                
+                foreach ($value as $index => $item) {
+                    $items[$index] = $this->normalizeIfSupported($item, $format, $context);
+                }
+                
+                $value = $items;
+            } else {
+                $value = $this->normalizeIfSupported($value, $format, $context);
             }
             
             $target = $binding ? ($binding->target ?? $binding->source ?? null) : $propertyMetadata->name;
@@ -54,6 +61,18 @@ class Normalizer implements NormalizerInterface
         }
         
         return $data;
+    }
+    
+    private function normalizeIfSupported($value, $format, array $context)
+    {
+        if ($this->supportsNormalization($value) 
+            && !in_array(spl_object_hash($value), $context['normalized'] ?? [])) {
+            $context['normalized'][] = spl_object_hash($value);
+            
+            return $this->normalize($value, $format, $context);
+        }
+        
+        return $value;
     }
 
     public function supportsNormalization($data, $format = null): bool
